9c48cdcda873afe51aa1f3a8300f07eb333b9f38
Alex DeBrie <alexdebrie1@gmail.com>
2014-11-24 23:20:09 -0600

    Add anaconda to /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/bin/python on slave nodes

--- anaconda/setup.sh
+++ anaconda/setup.sh
@@ -6,6 +6,6 @@ NEW_PATH="export PATH=/root/anaconda/bin:$PATH"
 
 # Need to figure out how to add /root/anaconda to $PATH on slave nodes.
 for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t -t $SSH_OPTS root@$node "echo 'export PATH=/root/anaconda/bin:$PATH' >> /root/.bash_profile"
+  ssh -t -t $SSH_OPTS root@$node "echo $NEW_PATH >> /root/.bash_profile"
   ssh -t -t $SSH_OPTS root@$node "source /root/.bash_profile"
 done
