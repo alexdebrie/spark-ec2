a267d07578fec41693764c7e55a04f9104c09998
Alex DeBrie <alexdebrie1@gmail.com>
2014-11-24 23:01:06 -0600

    Add anaconda to /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/bin/python on slave nodes

--- anaconda/setup.sh
+++ anaconda/setup.sh
@@ -2,7 +2,10 @@
 
 /root/spark-ec2/copy-dir /root/anaconda
 
+NEW_PATH="export PATH=/root/anaconda/bin:$PATH"
+
 # Need to figure out how to add /root/anaconda to $PATH on slave nodes.
 for node in $SLAVES $OTHER_MASTERS; do
+  ssh -t -t $SSH_OPTS root@$node "echo 'export PATH=/root/anaconda/bin:$PATH' >> /root/.bash_profile"
   ssh -t -t $SSH_OPTS root@$node "source /root/.bash_profile"
 done
