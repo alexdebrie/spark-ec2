59abe9aaf84ee4ca5e5ee256b370732bdb1d508f
Alex DeBrie <alexdebrie1@gmail.com>
2014-11-24 23:35:41 -0600

    Add anaconda to /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/bin/python on master node

--- anaconda/init.sh
+++ anaconda/init.sh
@@ -6,3 +6,6 @@ echo  "Downloading Anaconda......"
 wget -q http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh
 bash Anaconda-2.1.0-Linux-x86_64.sh -b
 rm -rf Anaconda-2.1.0-Linux-x86_64.sh
+
+echo "export PATH=/root/anaconda/bin:$PATH" >> /root/.bash_profile
+source /root/.bash_profile
