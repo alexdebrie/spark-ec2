6b168c431d23bf4c3ee796dc7db0fecd36c89de5
Alex DeBrie <alexdebrie1@gmail.com>
2014-12-01 21:40:25 -0600

    Fix some bugs in ipynb/init.sh.

--- ipynb/init.sh
+++ ipynb/init.sh
@@ -1,6 +1,6 @@
 export OPENSSL_CONF=/root/anaconda/ssl/openssl.cnf
 cd /root
-openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout .ipynbcert.pem -out .ipynbcert.pem
+openssl req -x509 -nodes -days 365 -subj '/' -newkey rsa:1024 -keyout .ipynbcert.pem -out .ipynbcert.pem
 
 ## Create ipython profile
 echo -e "Setting iPython settings...."
@@ -10,8 +10,8 @@ echo "c.NotebookApp.open_browser = False" >> /root/.ipython/profile_default/ipyt
 echo "c.NotebookApp.port = 8888" >> /root/.ipython/profile_default/ipython_notebook_config.py
 echo "c.NotebookApp.certfile = u'/root/.ipynbcert.pem'" >> /root/.ipython/profile_default/ipython_notebook_config.py
 echo "$IPYNB_PASSWORD" >> /root/.ipython/profile_default/nbpasswd.txt
-echo "PWDFILE='/root/.ipython/profile_default/nbpasswd.txt'"
-echo "c.NotebookApp.password = open(PWDFILE).read().strip()"
+echo "PWDFILE='/root/.ipython/profile_default/nbpasswd.txt'" >> /root/.ipython/profile_default/nbpasswd.txt
+echo "c.NotebookApp.password = open(PWDFILE).read().strip()" >> /root/.ipython/profile_default/nbpasswd.txt
 
 echo -e "import os\nos.environ['SPARK_HOME'] = '/root/spark'" >> /root/.ipython/profile_default/startup/00-pyspark-setup.py
 echo -e "import sys\nsys.path.insert(0, '/root/spark/python')" >> /root/.ipython/profile_default/startup/00-pyspark-setup.py
